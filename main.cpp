e43a3418778f82874e7be6cffd51328792a80ff1
Mohamed dhiaeddine Hassine <hassine-mohamed.dhiaeddine@isa.utm.md>
2025-11-06 14:49:01 +0200

    fucking around with double and float

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main (){
+    double pi = 3.145678986545678898765789 ;
+    std::cout <<"the result is :" << pi << std::endl  ;
+
+    float pi_1 = 3.145778900987667908680076;
+    std::cout<<"the result iss :" << pi_1<< std::endl ;
+    
+}
