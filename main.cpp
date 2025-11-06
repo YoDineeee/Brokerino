d9f871dd74b2322f178965e91d91a2abac8fecdb
Mohamed dhiaeddine Hassine <hassine-mohamed.dhiaeddine@isa.utm.md>
2025-11-06 14:50:52 +0200

    +long double

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ int main (){
     double pi = 3.145678986545678898765789 ;
     std::cout <<"the result is :" << pi << std::endl  ;
 
-    float pi_1 = 3.145778900987667908680076;
-    std::cout<<"the result iss :" << pi_1<< std::endl ;
-    
+    float pi_1 =  3.145678986545678898765789 ;
+    std::cout<<"the result is :" << pi_1<< std::endl ;
+
+    long double pi_2=  3.145678986545678898765789 ;
+        std::cout<<"the result is :" << pi_2<< std::endl ;
+
 }
