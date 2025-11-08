ea7acfbc0278c45cee4789d55fad456ef7d6fb1f
Mohamed dhiaeddine Hassine <hassine-mohamed.dhiaeddine@isa.utm.md>
2025-11-08 15:44:12 +0200

    Save local changes

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main (){
-  // lets assume that we have a situation where we can do the operation to calculate the persentage of the score when they ask
-  char Zigzag = -5;
-
-  std::cout<<Zigzag<< std::endl;
-
-}
