fdb3368a5272be120ac5ba7d0888d0b19eeac185
Mohamed dhiaeddine Hassine <hassine-mohamed.dhiaeddine@isa.utm.md>
2025-11-20 02:07:37 +0200

    notfinished_butcloseish

--- include/MQTT.h
+++ include/MQTT.h
@@ -1,8 +1,20 @@
 #ifndef BROKERINO_INCLUDE_MQTT_H_
 #define  BROKERINO_INCLUDE_MQTT_H_
-using namespace std ;
 #include<cstdint>
 #include <stdint.h>
+
+
+#define  CONNACK_BYTE  0x20
+#define PUBLISH_BYTE  0x30
+#define PUBACK_BYTE   0x40
+#define PUBREC_BYTE   0x50
+#define PUBREL_BYTE   0x60
+#define  PUBCOMP_BYTE  0x70
+#define SUBACK_BYTE   0x90
+#define  UNSUBACK_BYTE 0xB0
+#define PINGRESP_BYTE 0xD0
+
+
 
 
 // ---------------------------------------------------------------------------
@@ -17,19 +29,33 @@ using namespace std ;
 namespace Mqtt {
     enum class ControlPacketTypes:uint8_t{
         kReserved0 = 0,
+        //client only 
         kConnect = 1,
+        //server only 
         kConnack = 2, 
+        //client +server
         kPublish = 3,
+        //client
         kPuback = 4,
+        //server 
         kPubrec = 5,
+        //server
         kPubrel = 6,
+        //server 
         kPubcomp = 7,
+        //client
         kSubscribe = 8,
+        //server 
         kSuback = 9,
+        //client
         kUnsubscribe = 10,
+        //server 
         kUnsuback = 11,
+        //client 
         kPingreq = 12,
+        //server 
         kPingresp = 13,
+        //client
         kDisconnect = 14,
         kAuth = 15
 
@@ -58,10 +84,119 @@ struct Mqtt_Connect {
         unsigned char byte ;
         struct {
             
-        }
+        } bits ;
     }
+    struct {
+
+    }Payload;
 
 }
+
+
+struct Mqtt_Connack{
+     union  Mqtt_Header header ;
+     union {
+
+     }
+};
+
+
+struct Mqtt_Publish{
+     union  Mqtt_Header header ;
+     struct {
+
+     } 
+
+    }
+
+
+struct Mqtt_Puback{
+     union  Mqtt_Header header ;
+
+
+};
+
+
+struct Mqtt_Puberc{
+     union  Mqtt_Header header ;
+
+};
+
+
+struct Mqtt_Pubrel{
+     union  Mqtt_Header header ;
+};
+
+
+
+struct Mqtt_Pubcomp{
+     union  Mqtt_Header header ;
+
+};
+
+struct Mqtt_Subscribe{
+     union  Mqtt_Header header ;
+
+     struct {
+
+     }*truples;
+};
+
+struct Mqtt_Suback{
+     union  Mqtt_Header header ;
+};
+
+struct Mqtt_Suback{
+     union  Mqtt_Header header ;
+};
+
+
+struct Mqtt_Unsubscribe{
+    struct {
+
+    }*truples ;
+}
+
+struct Mqtt_Unsuback{
+     union  Mqtt_Header header ;
+};
+
+
+struct Mqtt_Pingreq{
+     union  Mqtt_Header header ;
+};
+
+
+
+struct Mqtt_Pingresp{
+    union  Mqtt_Header header ;
+
+};
+
+    
+
+
+
+struct Mqtt_Disconnect{
+    union  Mqtt_Header header ;
+};
+
+
+
+
+struct Mqtt_Auth{
+    union  Mqtt_Header header ;
+}; 
+
+
+
+
+
+
+
+
+
+
 
 
 
